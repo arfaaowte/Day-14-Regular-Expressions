8c07f75c57ad91542bed66f4c23afe7571ce695b
unknown <aowtearfa@gmail.com>
2020-08-29 09:48:02 +0530

    ADDED: author details

--- UC1.sh
+++ UC1.sh
@@ -1,3 +1,18 @@
+  GNU nano 4.9.3                     3_filpcoinsimulator.sh
+#****************************************
+#*
+#*Purpose : To take an User Registration and Validate the details which will be entered by User.
+#*
+#*
+#*
+#*
+#*
+#*
+#* @author Arfa
+#* @since 29/08/2020
+#*
+#********************************************/
+
 #!/bin/bash
 
 	#pattern to validate first name
